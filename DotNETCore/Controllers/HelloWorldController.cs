b5884b7776ca48e4732a0fe03246218514390c76
Tomas Foss Renaa <tomasrenaa@gmail.com>
2019-05-23 15:28:47 +0200

    playing around

---
+++ DotNETCore/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNETCore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        // Get: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        // /[Controller]/[ActionName]/[Parameters]
+
+    }
+}
