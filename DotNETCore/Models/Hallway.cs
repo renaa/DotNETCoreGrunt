08c21384e72f6f33ce81ef0f3d6568841678bfe8
renaa <tomasrenaa@gmail.com>
2019-07-17 21:30:42 +0200

    cleanup

---
+++ DotNETCore/Models/Hallway.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNETCore.Models
+{
+    public class Hallway
+    {
+        public string Name { get; set; }
+
+    }
+}
